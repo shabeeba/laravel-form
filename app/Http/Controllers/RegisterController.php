0a2b440224657ecb8d3ce9249f01a4244b2ef7bc
Fathimasabeeba <fsabeeba@gmail.com>
2024-10-08 07:56:07 +0400

    Validation Added

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -12,6 +12,13 @@ return view("newform");
     }
     public function register(Request $request)
     {
+        $request->validate(
+           [
+             'name' => "required",
+             'email' => "required|email",
+             'password' => "required"
+           ]
+           );
         echo $request->name;
         echo $request->email;
         echo $request->password;
--- resources/views/newform.blade.php
+++ resources/views/newform.blade.php
@@ -10,21 +10,37 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
-   <form action="{{url('/')}}/register" method="POST">
-    @csrf 
-    <div class="container" >  
-   <div class="form-group ">
-     <label for="">Name</label>
-     <input type="text" name="name" id="" class="form-control" placeholder="yourname" aria-describedby="helpId">
-   </div>
-   <div class="form-group">
-    <label for="">Email</label>
-    <input type="email" name="email" id="" class="form-control" placeholder="Mailid" aria-describedby="helpId">
-  </div>
-  <div class="form-group">
-    <label for="">Password</label>
-    <input type="password" name="password" id="" class="form-control" placeholder="password" aria-describedby="helpId">
-  </div>
+   
+    <form action="{{url('/')}}/register" method="POST">
+      @csrf 
+      <div class="container" >  
+     <div class="form-group ">
+       <label for="">Name </label>
+       <input type="text" name="name" id="" value="{{old('name')}}" class="form-control" placeholder="yourname" aria-describedby="helpId">
+       <span class="text-danger">
+        @error('name')
+          {{$message}}
+        @enderror
+       </span>
+     </div>
+     <div class="form-group">
+      <label for="">Email</label>
+      <input type="email" name="email" id="" value="{{old('email')}}"  class="form-control" placeholder="Mailid" aria-describedby="helpId">
+      <span class="text-danger">
+        @error('email')
+          {{$message}}
+        @enderror
+       </span>
+    </div>
+    <div class="form-group">
+      <label for="">Password</label>
+      <input type="password" name="password" id="" class="form-control" placeholder="password" aria-describedby="helpId">
+      <span class="text-danger">
+        @error('password')
+          {{$message}}
+        @enderror
+       </span>
+    </div>
 <button class="btn btn-primary">
     Submit
 </button>
